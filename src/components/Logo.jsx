67d55603be084b96d2b016a108e1125020c55c86
ferdouszihad <f.zihad333@gmail.com>
2025-05-05 20:58:02 +0600

    navbar added

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router";
 import logo from "../assets/logo.png";
 const Logo = () => {
   return (
-    <div className="flex items-center gap-2 text-2xl font-bold text-info ">
+    <Link
+      to="/"
+      className="flex items-center gap-2 text-2xl font-bold text-info "
+    >
       <img className="w-8 relative left-5 bottom-2" src={logo} alt="" />
       <h2>ZapShift</h2>
-    </div>
+    </Link>
   );
 };
 
