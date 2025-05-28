027ec8bd73383c0e818e2b9a72961945f20875cb
ferdouszihad <f.zihad333@gmail.com>
2025-05-28 16:25:01 +0600

    fix page title bg issue

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -1,6 +1,6 @@
 const PageTitle = ({ title, subtitle }) => {
   return (
-    <div className="rounded-xl text-white bg-secondary my-5 bg-[url(be-amerchant-bg.png)] p-5">
+    <div className="rounded-xl text-white bg-secondary my-5 bg-[url(/be-amerchant-bg.png)] p-5">
       <h2 className="text-4xl font-bold">{title}</h2>
       <p className="text-base-300">{subtitle}</p>
     </div>
